0d7a25591610c43c1d0ac7951721a053f6f7b818
caulok <ramicaulo@gmail.com>
2022-11-13 20:33:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let altCm = 174;
+let altMts = 1.74;
+let peso = 70.3;
+let altMtsRedondeada = Math.round(altMts);
+let pesoRedondeado = Math.floor(peso);
+let comparacion = Number.MAX_VALUE + 1 == Number.MAX_VALUE; 
